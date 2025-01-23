8b7598020e27a95ae6f606abe498fc54102ad776
Daniel Capuria <Capuria92@yahoo.com>
2025-01-23 18:10:15 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 class="text-2xl font-bold text-blue-500 mb-4">Big project </h1>
+      
+    </div>
+  );
+}
+
+export default App;
